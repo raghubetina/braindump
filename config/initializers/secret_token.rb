228741d1f0812dd49934111e599f5b0fcb258df0
Raghu Betina <rbetina@gmail.com>
2013-02-14 11:39:00 -0600

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainDump::Application.config.secret_token = '42d63474d484a944a860523e724d5324d91a8592339a819615207b368fb48eefbba928b0167f7db91ddc9f9126b0157721da449359cb81093117b5325845ffcd'
